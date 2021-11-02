84c67d798cd33f539fe04d9df8bdec58ea460b93
Marcel Schüren <marcelschueren@gmx.de>
2021-11-02 15:33:55 +0100

    Test for CI II

--- backend/src/main/java/de/neufische/mydepot/MyDepotApplication.java
+++ backend/src/main/java/de/neufische/mydepot/MyDepotApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyDepotApplication {
 
     public static void main(String[] args) {
-        //Test
+        //Test II
         SpringApplication.run(MyDepotApplication.class, args);
     }
 }
