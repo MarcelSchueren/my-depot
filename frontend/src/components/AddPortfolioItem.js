2ea5afeca8b9dbff3ceb4a2aacf899b08703e16b
Marcel Schüren <marcelschueren@gmx.de>
2021-11-08 15:12:17 +0100

    First little refactor of frontend newPortfolio

--- frontend/src/components/AddPortfolioItem.js
+++ frontend/src/components/AddPortfolioItem.js
@@ -1,13 +1,10 @@
-import {Button, Checkbox, Container, FormControlLabel, FormGroup, Grid, TextField, Typography} from "@mui/material";
+import {Button, Checkbox, FormControlLabel, FormGroup, TextField, Typography} from "@mui/material";
 import {useState} from "react";
-import PortfolioItem from "./PortfolioItem";
-import useStyles from "../hooks/useStyles";
 
-export default function AddPortfolioItem({activePortfolioItem, clearActivePortfolioItem}) {
+export default function AddPortfolioItem({activePortfolioItem, clearActivePortfolioItem, portfolioItems, setPortfolioItems}) {
 
-    const [portfolioItems, setPortfolioItems] = useState([])
+
     const [quantityPortfolioItem, setQuantityPortfolioItem] = useState(0)
-    const classes = useStyles()
 
     if (!activePortfolioItem) {
         return <div></div>
@@ -51,20 +48,6 @@ export default function AddPortfolioItem({activePortfolioItem, clearActivePortfo
                 </FormGroup>
                 <Button variant="outlined" onClick={addPortfolioItem}>Add</Button>
             </form>
-
-
-            {portfolioItems.length === 0 ? <div></div> :
-                <Container className={classes.cardGrid}>
-                    <Grid container spacing={2}>
-                        {portfolioItems.map(stock => {
-                            return (
-                                <Grid item key={stock.id} xs={12} sm={6} md={4}>
-                                    <PortfolioItem stock={stock} key={stock.id}/>
-                                </Grid>
-                            )
-                        })}
-                    </Grid>
-                </Container>}
         </section>
     )
 }
--- frontend/src/pages/NewDepotPage.js
+++ frontend/src/pages/NewDepotPage.js
@@ -1,17 +1,19 @@
-import {TextField, Typography} from "@mui/material";
+import {Container, Grid, TextField, Typography} from "@mui/material";
 import useStyles from "../hooks/useStyles";
 import {useState} from "react";
 import {getPortfolioItem} from "../service/depot-api-service";
 import AddPortfolioItem from "../components/AddPortfolioItem";
+import PortfolioItem from "../components/PortfolioItem";
 
 export default function NewDepotPage() {
 
     const [symbol, setSymbol] = useState()
     const [activePortfolioItem, setActivePortfolioItem] = useState()
+    const [portfolioItems, setPortfolioItems] = useState([])
 
     const classes = useStyles()
 
-    const clearActivePortfolioItem = () => {
+        const clearActivePortfolioItem = () => {
         setActivePortfolioItem()
         setSymbol('')
     }
@@ -32,7 +34,23 @@ export default function NewDepotPage() {
                 <Typography variant="h5" gutterBottom>Add a stock:</Typography>
                 <TextField id="stockName" label="Enter symbol" variant="outlined" value={symbol} onChange={event => setSymbol(event.target.value)}/>
             </form>
-            <AddPortfolioItem activePortfolioItem={activePortfolioItem} clearActivePortfolioItem={clearActivePortfolioItem}/>
+            <AddPortfolioItem
+                activePortfolioItem={activePortfolioItem}
+                clearActivePortfolioItem={clearActivePortfolioItem}
+                portfolioItems={portfolioItems}
+                setPortfolioItems={setPortfolioItems}/>
+            {portfolioItems.length === 0 ? <div></div> :
+                <Container className={classes.cardGrid}>
+                    <Grid container spacing={2}>
+                        {portfolioItems.map(stock => {
+                            return (
+                                <Grid item key={stock.id} xs={12} sm={6} md={4}>
+                                    <PortfolioItem stock={stock} key={stock.id}/>
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
+                </Container>}
         </div>
     )
 }
