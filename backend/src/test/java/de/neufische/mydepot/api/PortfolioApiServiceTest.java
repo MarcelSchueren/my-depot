e0eeacdd85abc8c68125f620688817c05a377b1d
Marcel Schüren <marcelschueren@gmx.de>
2021-11-14 14:08:10 +0100

    Fix one test

--- backend/src/test/java/de/neufische/mydepot/api/PortfolioApiServiceTest.java
+++ backend/src/test/java/de/neufische/mydepot/api/PortfolioApiServiceTest.java
@@ -20,55 +20,67 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class PortfolioApiServiceTest {
 
-//    @MockBean
-//    private YahooFinanceService yahooFinanceService;
-//
-//    @Autowired
-//    private PortfolioApiService portfolioApiService;
-//
-//    @Test
-//    @DisplayName("Should update a given portfolio with one item")
-//    void updateAll() {
-//        //GIVEN
-//
-//        PortfolioItem amazon = new PortfolioItem(
-//                "1"
-//                , "Amazon"
-//                , "AMZN"
-//                , 3
-//                , 500
-//                , BigDecimal.valueOf(1)
-//                , BigDecimal.valueOf(0));
-//
-//        List<PortfolioItem> all = List.of(amazon);
-//
-//        String[] symbols = {"AMZN"};
-//        Stock stock1 = new Stock("AMZN");
-//        StockQuote stockQuote = new StockQuote("AMZN");
-//        stock1.setQuote(stockQuote);
-//
-//        Map<String, Stock> stocks = new HashMap<>(Map.of("AMZN", stock1));
-//
-//        when(yahooFinanceService.get(symbols)).thenReturn(stocks);
-//        when(yahooFinanceService.getRegularMarketPrice(stocks, amazon)).thenReturn(BigDecimal.valueOf(550));
-//        when(yahooFinanceService.getRegularMarketChangePercent(stocks, amazon)).thenReturn(BigDecimal.valueOf(10));
-//
-//        PortfolioItem amazonUpdated = new PortfolioItem(
-//                "1"
-//                , "Amazon"
-//                , "AMZN"
-//                , 3
-//                , 500
-//                , BigDecimal.valueOf(550)
-//                , BigDecimal.valueOf(10));
-//
-//        List<PortfolioItem> expected = List.of(amazonUpdated);
-//
-//        //WHEN
-//
-//        List<PortfolioItem> actual = portfolioApiService.updateAll(all);
-//
-//        //THEN
-//        assertEquals(expected, actual);
-//    }
+    @MockBean
+    private YahooFinanceService yahooFinanceService;
+
+    @Autowired
+    private PortfolioApiService portfolioApiService;
+
+    @Test
+    @DisplayName("Should update a given portfolio with one item")
+    void updateAll() {
+        //GIVEN
+
+        PortfolioItem amazon = PortfolioItem.builder()
+                .id("1")
+                .displayName("Amazon")
+                .symbol("AMZN")
+                .quantity(3)
+                .boughtAtPricePerShare(500)
+                .build();
+
+        List<PortfolioItem> all = List.of(amazon);
+
+        String[] symbols = {"AMZN"};
+        Stock stock1 = new Stock("AMZN");
+        StockQuote stockQuote = new StockQuote("AMZN");
+        stock1.setQuote(stockQuote);
+
+        Map<String, Stock> stocks = new HashMap<>(Map.of("AMZN", stock1));
+
+        when(yahooFinanceService.get(symbols)).thenReturn(stocks);
+        when(yahooFinanceService.getRegularMarketPrice(stocks, amazon)).thenReturn(BigDecimal.valueOf(550));
+        when(yahooFinanceService.getRegularMarketChangePercent(stocks, amazon)).thenReturn(BigDecimal.valueOf(10));
+        when(yahooFinanceService.getDayLow(stocks, amazon)).thenReturn(BigDecimal.valueOf(2));
+        when(yahooFinanceService.getDayHigh(stocks, amazon)).thenReturn(BigDecimal.valueOf(3));
+        when(yahooFinanceService.getYearLow(stocks, amazon)).thenReturn(BigDecimal.valueOf(1));
+        when(yahooFinanceService.getYearHigh(stocks, amazon)).thenReturn(BigDecimal.valueOf(10));
+        when(yahooFinanceService.getDividend(stocks, amazon)).thenReturn(BigDecimal.valueOf(0));
+
+
+        PortfolioItem amazonUpdated = PortfolioItem.builder()
+                .id("1")
+                .displayName("Amazon")
+                .symbol("AMZN")
+                .quantity(3)
+                .boughtAtPricePerShare(500)
+                .regularMarketPrice(BigDecimal.valueOf(550))
+                .regularMarketChangePercent(BigDecimal.valueOf(10))
+                .dayLow(BigDecimal.valueOf(2))
+                .dayHigh(BigDecimal.valueOf(3))
+                .yearLow(BigDecimal.ONE)
+                .yearHigh(BigDecimal.valueOf(10))
+                .dividend(BigDecimal.ZERO)
+
+                .build();
+
+        List<PortfolioItem> expected = List.of(amazonUpdated);
+
+        //WHEN
+
+        List<PortfolioItem> actual = portfolioApiService.updateAll(all);
+
+        //THEN
+        assertEquals(expected, actual);
+    }
 }
